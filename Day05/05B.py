c6c08c3f48b78925969abe31340816655197994f
LandmineFlipFlop <elystanmay@gmail.com>
2025-10-24 08:33:14 -0700

    day five 2015 is complete

--- Day05/05B.py
+++ Day05/05B.py
@@ -1,10 +1,10 @@
 import sys, time, copy
 sys.path.append("../useful_functions.py")
 from useful_functions import *
-with open ('05_data/test_data', 'r') as casefile:
-    lines = casefile.read().splitlines()
-# with open ('05_data/full_data', 'r') as casefile:
+# with open ('05_data/test_data', 'r') as casefile:
 #     lines = casefile.read().splitlines()
+with open ('05_data/full_data', 'r') as casefile:
+    lines = casefile.read().splitlines()
 startTime = time.time()
 count = 0
 
@@ -14,15 +14,17 @@ for line in lines:
     repeated = False
     while i < len(line):
 
-        if i < len(line) - 1:
-            if line[i] + line[i + 1] in line[0:i - 1] and i != 0:
+        if i < len(line) - 1 and i != 0:
+            if line[i] + line[i + 1] in line[0:i]:
                 pair = True
                 print('    ' + blue(line), yellow(line[i] + line[i + 1]))
+
 
         if i < len(line) - 2:
             if line[i] == line[i + 2]:
                 repeated = True
-                print('    ' + darkcyan(line), cyan(line[i] + line[i+1] + line[i+2]))
+                print('    ' + darkcyan(line), yellow(line[i] + line[i+1] + line[i+2]))
+
         i += 1
 
     if pair and repeated:
