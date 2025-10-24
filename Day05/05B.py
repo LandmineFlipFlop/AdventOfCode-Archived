b129d2b675a265814887566e059689df4e4c093b
LandmineFlipFlop <elystanmay@gmail.com>
2025-10-24 08:20:08 -0700

    day five 2015 part two progress

---
+++ Day05/05B.py
@@ -0,0 +1,36 @@
+import sys, time, copy
+sys.path.append("../useful_functions.py")
+from useful_functions import *
+with open ('05_data/test_data', 'r') as casefile:
+    lines = casefile.read().splitlines()
+# with open ('05_data/full_data', 'r') as casefile:
+#     lines = casefile.read().splitlines()
+startTime = time.time()
+count = 0
+
+for line in lines:
+    i = 0
+    pair = False
+    repeated = False
+    while i < len(line):
+
+        if i < len(line) - 1:
+            if line[i] + line[i + 1] in line[0:i - 1] and i != 0:
+                pair = True
+                print('    ' + blue(line), yellow(line[i] + line[i + 1]))
+
+        if i < len(line) - 2:
+            if line[i] == line[i + 2]:
+                repeated = True
+                print('    ' + darkcyan(line), cyan(line[i] + line[i+1] + line[i+2]))
+        i += 1
+
+    if pair and repeated:
+        count += 1
+        print(green(line))
+    else:
+        print(red(line))
+
+
+print(green("count: " + str(count)))
+timerstop(startTime)
